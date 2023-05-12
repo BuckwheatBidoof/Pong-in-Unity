c76836286620605caf9e399b8f1ed400c60b8887
BuckwheatBidoof <p.bejar.christopher@gmail.com>
2023-05-12 15:00:06 -0500

    Logic script to add score

---
+++ Pong/Assets/Scripts/LogicScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public Text leftScore;
+    public Text rightScore;
+    private int playerScore = 0;
+    private int enemyScore = 0;
+    public AudioSource score;
+    public GameObject ball;
+    private Rigidbody2D rb;
+    public void addScore(int player) {
+        if(player == 0) {
+            playerScore++;
+            leftScore.text = playerScore.ToString();
+        } else {
+            enemyScore++;
+            rightScore.text = enemyScore.ToString();
+        }
+        score.Play();
+
+        rb = ball.GetComponent<Rigidbody2D>();
+
+        rb.velocity = Vector2.zero;
+        ball.transform.position = new Vector2(0,0);
+
+        if(playerScore == 10) {
+
+        } else if(enemyScore == 10) {
+
+        }
+        
+    }
+}
