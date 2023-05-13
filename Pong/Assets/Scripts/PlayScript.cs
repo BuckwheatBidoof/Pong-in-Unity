f8afdd7351a5f4eae9c29297ac8adefa0f18ec2f
BuckwheatBidoof <p.bejar.christopher@gmail.com>
2023-05-12 21:10:35 -0500

    Script to replay game after a win or loss

--- Pong/Assets/Scripts/PlayScript.cs
+++ Pong/Assets/Scripts/PlayScript.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayScript : MonoBehaviour
 {
-    
+  public GameObject win;
+  public GameObject lose;
+  void Start()
+  {
+    if (LogicScript.playerScore == 10)
+    {
+      win.SetActive(true);
+    }
+    else
+    {
+      lose.SetActive(true);
+    }
+  }
+
+  public void startNewGame()
+  {
+    SceneManager.LoadScene("Game");
+  }
 }
