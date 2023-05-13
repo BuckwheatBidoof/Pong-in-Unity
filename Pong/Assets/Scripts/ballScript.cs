0ac07e7200d6c39fec9f91ec733b758ebcadc445
BuckwheatBidoof <p.bejar.christopher@gmail.com>
2023-05-12 20:49:20 -0500

    Improved physics and removed rng of direction

--- Pong/Assets/Scripts/ballScript.cs
+++ Pong/Assets/Scripts/ballScript.cs
@@ -4,64 +4,107 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour
 {
-    public bool startOfRound = true;
-    private int startDirection;
-    private Vector2 direction = new Vector2(0, 0);
-    private Rigidbody2D rb;
-    public int speed = 150;
-    public AudioSource paddle;
-    public AudioSource wall;
-    public GameObject logicSystem;
-    private LogicScript logic;
+  public bool startOfRound = true;
+  private int startDirection;
+  private Vector2 direction = new Vector2(0, 0);
+  private Rigidbody2D rb;
+  private Rigidbody2D rbl;
+  private Rigidbody2D rbr;
+  public float speed = 150;
+  public AudioSource paddle;
+  public AudioSource wall;
+  public GameObject logicSystem;
+  private LogicScript logic;
+  public GameObject leftPaddle;
+  public GameObject rightPaddle;
+  private float topSpeed = 15;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    rbl = leftPaddle.GetComponent<Rigidbody2D>();
+    rbr = rightPaddle.GetComponent<Rigidbody2D>();
+    logic = logicSystem.GetComponent<LogicScript>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    StartCoroutine(waiter());
+
+    if (transform.position.x > 9.5)
     {
-        rb = GetComponent<Rigidbody2D>();
-        logic = logicSystem.GetComponent<LogicScript>();
+      logic.addScore(0);
+      startOfRound = true;
     }
-
-    // Update is called once per frame
-    void Update()
+    else if (transform.position.x < -9.5)
     {
-        StartCoroutine(waiter());
-        
-        if(transform.position.x > 10) {
-            logic.addScore(0);
-            startOfRound = true;
-        } else if(transform.position.x < -10) {
-            logic.addScore(1);
-            startOfRound = true;
-        }
-        
+      logic.addScore(1);
+      startOfRound = true;
     }
 
-    IEnumerator waiter() {
-        if(startOfRound) {
-            startDirection = Random.Range(0, 2);
-            startOfRound = false;
-
-            yield return new WaitForSeconds(2);
-
-            if(startDirection == 0) {
-                direction = new Vector2(-speed,0);
-            } else if(startDirection == 1) {
-                direction = new Vector2(speed,0);
-            }
-
-            rb.AddForce(direction);
-        }
+    Debug.Log("rb.velocity: " + rb.velocity);
+    if (rb.velocity.magnitude > topSpeed)
+    {
+      rb.velocity = Vector2.ClampMagnitude(rb.velocity, topSpeed);
+      speed = 750;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.layer == 3) {
-            paddle.Play();
-            direction = new Vector2(-direction.x, Random.Range(-speed, speed));
-            rb.AddForce(direction);
-        }else if(collision.gameObject.layer == 6) {
-            wall.Play();
-            direction = new Vector2(direction.x, -direction.y);
-            rb.AddForce(direction);
-        }
+  }
+
+  IEnumerator waiter()
+  {
+    if (startOfRound)
+    {
+      startDirection = Random.Range(0, 2);
+      startOfRound = false;
+      speed = 150;
+
+      yield return new WaitForSeconds(2);
+
+      if (startDirection == 0)
+      {
+        direction = new Vector2(-speed, 0);
+      }
+      else if (startDirection == 1)
+      {
+        direction = new Vector2(speed, 0);
+      }
+
+      rb.AddForce(direction);
     }
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.layer == 3)
+    {
+      paddle.Play();
+      speed *= 1.5f;
+      Debug.Log("Speed: " + speed);
+      if (collision.gameObject.tag == "Player")
+      {
+        direction = new Vector2(speed, rb.velocity.y * rbl.velocity.y * 2);
+      }
+      else
+      {
+        direction = new Vector2(-speed, rb.velocity.y * rbr.velocity.y * 2);
+      }
+      rb.AddForce(direction);
+    }
+    else if (collision.gameObject.layer == 6)
+    {
+      wall.Play();
+      if (transform.position.y > 0)
+      {
+        direction = new Vector2(direction.x / 2, -speed);
+      }
+      else
+      {
+        direction = new Vector2(direction.x / 2, speed);
+      }
+      rb.AddForce(direction);
+    }
+  }
 }
